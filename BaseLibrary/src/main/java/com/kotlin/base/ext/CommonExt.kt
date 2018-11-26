cb3f4b0a5a2b8d3396808b7e25efd0e942522490
Moon ch <changjunjie07@163.com>
2018-11-26 16:09:41 +0800

    build

--- BaseLibrary/src/main/java/com/kotlin/base/ext/CommonExt.kt
+++ BaseLibrary/src/main/java/com/kotlin/base/ext/CommonExt.kt
@@ -1,14 +1,17 @@
 package com.kotlin.base.ext
 
+
 import com.kotlin.base.rx.BaseSubscriber
+import com.trello.rxlifecycle.LifecycleProvider
 import rx.Observable
-import rx.Scheduler
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
 
-fun <T> Observable<T>.execute(subscriber :BaseSubscriber<T>) {
-    this.observeOn(AndroidSchedulers.mainThread())
-            .subscribeOn(Schedulers.io())
+fun <T> Observable<T>.execute(subscriber: BaseSubscriber<T>,
+                              lifecycleProvider: LifecycleProvider<*>) {
+    this.subscribeOn(Schedulers.io())
+            .compose(lifecycleProvider.bindToLifecycle())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(subscriber)
 }
--- UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/kotlin/user/ui/activity/RegisterActivity.kt
@@ -14,7 +14,6 @@ class RegisterActivity : BaseMvpActivity<RegisterPresenter>(), RegisterView{
     override fun onRegisterResult(result: Boolean) {
         toast("注册成功")
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
